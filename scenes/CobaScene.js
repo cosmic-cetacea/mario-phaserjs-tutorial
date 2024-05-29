f3565cda4dadb7d8e1d0716fff80c316efbe0c0d
Cosmic Cetacea <cosmic.cetacea@gmail.com>
2024-05-29 09:56:30 +0700

    minimum viable product

--- scenes/CobaScene.js
+++ scenes/CobaScene.js
@@ -2,6 +2,8 @@ import Phaser from 'phaser';
 import awan from '/assets/cloud.png';
 import mario from '/assets/mario_spritesheet.png';
 import tanah from '/assets/ground-long.png';
+import koin from '/assets/coin.png';
+import tring from '/assets/smb_coin.wav';
 
 
 export default class CobaScene extends Phaser.Scene {
@@ -13,16 +15,24 @@ export default class CobaScene extends Phaser.Scene {
     this.load.image('awan', awan);
     this.load.image('tanah', tanah);
     this.load.spritesheet('mario', mario, {frameWidth: 16, frameHeight: 32});
+    this.load.image('koin', koin);
+    this.load.audio('tring', tring);
   }
 
   create(){
+    this.score = 0;
     this.awan1 = this.add.image(100, 100, 'awan');
     this.awan2 = this.add.image(200, 50, 'awan');
     this.awan3 = this.add.image(300, 70, 'awan');
     this.awan4 = this.add.image(400, 100, 'awan');
 
     this.tanah = this.physics.add.staticGroup();
-    this.tanah.create(100, 300, 'tanah');
+    this.tanah.create(80, 350, 'tanah');
+    this.tanah.create(300, 290, 'tanah');
+    this.tanah.create(500, 250, 'tanah');
+    this.scoreText = this.add.text(20, 20, 'Score: 0', {fontSize: '28px'});
+    this.koin = this.physics.add.image(50, 280, 'koin');
+    this.suaraKoin = this.sound.add('tring');
 
     this.player = this.physics.add.sprite(150, 100, 'mario', 0);
     this.anims.create({
@@ -30,19 +40,56 @@ export default class CobaScene extends Phaser.Scene {
       frames: this.anims.generateFrameNumbers('mario', {start: 2, end: 4}),
       repeat: -1,
       frameRate: 10,
-
     });
 
     this.anims.create({
-      key: 'stop',
+      key: 'stop-kanan',
       frames: [{key: 'mario', frame: 0}],
       frameRate: 10,
     });
 
+    
+    this.anims.create({
+      key: 'stop-kiri',
+      frames: [{key: 'mario', frame: 23}],
+      frameRate: 10,
+    });
+
+    this.anims.create({
+      key: 'lompat-kanan',
+      frames: [{key: 'mario', frame: 11}],
+      frameRate: 10,
+    });
+
+
+    this.anims.create({
+      key: 'lompat-kiri',
+      frames: [{key: 'mario', frame: 12}],
+      frameRate: 10,
+    });
+
+    this.anims.create({
+      key: 'kiri',
+      frames: [{key: 'mario', frame: 21}, {key: 'mario', frame:20}, {key: 'mario', frame: 19}],
+      repeat: -1,
+      frameRate: 10,
+    });
+
     this.keyboard = this.input.keyboard.createCursorKeys();
+    this.cameras.main.startFollow(this.player);
 
 
     this.physics.add.collider(this.player, this.tanah);
+    this.physics.add.collider(this.koin, this.tanah);
+    this.physics.add.overlap(this.player, this.koin, this.ambilKoin, null, this);
+    
+  }
+
+  ambilKoin(player, koin){
+    this.score += 100;
+    this.suaraKoin.play();
+    koin.destroy();
+    this.scoreText.setText(`Score: ${this.score}`);
   }
 
   update(){
@@ -51,12 +98,29 @@ export default class CobaScene extends Phaser.Scene {
       this.player.setVelocityX(100);
     }
     else if (this.keyboard.left.isDown){
-      this.player.setFlipX(true);
+      this.player.anims.play('kiri', true);
       this.player.setVelocityX(-100);
     }
     else {
-      this.player.setVelocityX(0);
-      this.player.anims.play('stop');
+      if (this.player.body.facing == 14){
+        this.player.anims.play('stop-kanan');
+        this.player.setVelocityX(0);
+      }
+      else if (this.player.body.facing == 13){
+        this.player.anims.play('stop-kiri');
+        this.player.setVelocityX(0);
+      }
+    }
+
+    if (this.keyboard.up.isDown && this.player.body.touching.down){
+      this.player.setVelocityY(-200);
+    }
+
+    if (!this.player.body.touching.down && this.keyboard.right.isDown){
+      this.player.anims.play('lompat-kanan');
+    }
+    if (!this.player.body.touching.down && this.keyboard.left.isDown){
+      this.player.anims.play('lompat-kiri');
     }
 
   }
